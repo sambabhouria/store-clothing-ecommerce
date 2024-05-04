6071f365e946339a806d0fbc9e7e3eca03b31ed5
Samba Diallo <sambadiallo@macbook-pro-de-sambabhouria.home>
2024-05-04 20:05:55 +0200

    store cloth ecommerce site

---
+++ src/component/shimmer/Shimmer.jsx
@@ -0,0 +1,10 @@
+import './Shimmer.css'
+function Shimmer() {
+  return (
+    <div className="shimmer">
+      <div className="shimmer-animation"></div>
+    </div>
+  )
+}
+
+export default Shimmer
